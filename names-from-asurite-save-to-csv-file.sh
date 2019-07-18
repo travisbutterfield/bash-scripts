13fde8bdbdce125b3d1d27ad9f97983491836b3a
Travis Butterfield <travisbutterfield@gmail.com>
2019-07-18 13:47:36 -0700

    test1

--- names-from-asurite-save-to-csv-file.sh
+++ names-from-asurite-save-to-csv-file.sh
@@ -8,7 +8,7 @@ printf "Please prepare a .txt file with ASURITE IDs one-per-line\n\n"
 #Must use full path without tilde or $HOME variable to work
 IFS= read -r -p "Enter the full path to the file you have prepared: " path
 
-printf "You will get a new .csv file with the desired Full Names in your Home directory named 'names-output.csv'\n\n"
+printf "You will get a new .csv file with the desired names in your Home directory named 'names-output.csv'\n\n"
 
 printf "Please wait while I process the file.\n\n"
 
