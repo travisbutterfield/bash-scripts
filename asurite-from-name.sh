47719ed3cdb0a35badf403455b35324576fa473a
Travis Butterfield <travisbutterfield@gmail.com>
2019-10-24 12:11:22 -0700

    update asurite-from-name.sh to work on a Mac

--- asurite-from-name.sh
+++ asurite-from-name.sh
@@ -6,7 +6,7 @@ read -p "Affiliate First Name: " fname
 
 read -p "Affiliate Last Name: " lname
 
-asuriteId=$(lynx -dump https://asudir-solr.asu.edu/asudir/directory/select?q=firstName:$fname%20AND%20lastName:$lname | sed -E 's/\<str>/&\n/g' | grep asuriteId | sed 's/<str name="asuriteId">//' | sed 's\</str>\\')
+asuriteId=$(lynx -source https://asudir-solr.asu.edu/asudir/directory/select?q=firstName:$fname%20AND%20lastName:$lname | sed -E 's/\<str>/&\n/g' | grep asuriteId | sed 's/<str name="asuriteId">//' | sed 's\</str>\\')
 
 printf "ASURITE ID: \e[1m\e[36m$asuriteId\n"
 printf "\n"
