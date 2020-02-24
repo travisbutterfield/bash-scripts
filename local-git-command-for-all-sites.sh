54d054ec2db20f1da44d44c70ac8a21d6496f538
Travis Butterfield <travisbutterfield@gmail.com>
2020-02-24 15:24:14 -0700

    update local-git-command-for-all-sites.sh to run parallel in tabs

--- local-git-command-for-all-sites.sh
+++ local-git-command-for-all-sites.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 printf "\nHello!\n\nWelcome to my bash script for doing mass local git operations on all of\nyour downloaded Pantheon sites!\n\nPlease enter your full git command below.\n\n"
-
+IFS='\n'
 read -p 'Enter command: ' command
 
-declare -a arr="($(ls /var/www/html/ | grep -v "\."))"
-# # declare -a arr="(crim)"
+#declare -a arr="($(ls /var/www/html/ | grep -v "\."))"
+declare -a arr="(crim crd)"
 
 # #printf '%s\n' "${arr[@]}"
 
 for i in "${arr[@]}"
 do
-  cd /var/www/html/$i; printf "..... $i .....\n"; eval $command | head -n 15; printf "....................\n\n"; #read -p 'press any key to continue';
+  gnome-terminal --tab --title="$i" -- bash -c 'cd /var/www/html/'"$i"'; printf "..... '"$i"' .....\n"; eval '"$command"' | head -n 15; printf "....................\n\n";$SHELL'
 done
 
 unset arr
