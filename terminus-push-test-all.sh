6d3aeceb04bf8849dfebef3dddf7aa4fb09b58d8
Travis Butterfield <travisbutterfield@gmail.com>
2020-09-09 14:21:38 -0700

    update terminus-push-test-all.sh

--- terminus-push-test-all.sh
+++ terminus-push-test-all.sh
@@ -4,9 +4,9 @@ printf "\nHello!\n\nWelcome to my bash script for doing mass git push operations
 IFS='\n'
 read -p 'Please add a note for this mass deployment: ' note
 
-declare -a arr="($(terminus site:list --team --fields=name,framework | sed -e '/wordpress/d' | sed -e '/drupal8/d' | sed -e '/morrison-institute/d' | sed -n '1,3 !p' | sed -n '$ !p' | tr -s ' ' | cut -d ' ' -f-2))"
+# declare -a arr="($(terminus site:list --team --fields=name,framework | sed -e '/wordpress/d' | sed -e '/drupal8/d' | sed -e '/morrison-institute/d' | sed -n '1,3 !p' | sed -n '$ !p' | tr -s ' ' | cut -d ' ' -f-2))"
 #create a custom array by uncommenting and editing the next line:
-# declare -a arr="(copp crim crd spa socialwork)"
+declare -a arr="(spa crim cvpcsd7 morrison2017 crd cemhs)"
 
 #uncomment next line to check array
 # printf '%s\n' "${arr[@]}"
