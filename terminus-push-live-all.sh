f78b4c295240e07873b5a520b0bee32f57703ec1
Travis Butterfield <travisbutterfield@gmail.com>
2020-09-09 14:19:45 -0700

    update terminus-push-live-all.sh

--- terminus-push-live-all.sh
+++ terminus-push-live-all.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 printf "\nHello!\n\nWelcome to my bash script for doing mass git push operations on all of your Pantheon sites (from test to live)!\n\n"
-IFS='\n'
+IFS=$'\n'
 read -p 'Please add a note for this mass deployment: ' note
 
-declare -a arr="($(terminus site:list --team --fields=name,framework | sed -e '/wordpress/d' | sed -e '/drupal8/d' | sed -n '1,3 !p' | sed -n '$ !p' | tr -s ' ' | cut -d ' ' -f-2))"
+# declare -a arr="($(terminus site:list --team --fields=name,framework | sed -e '/wordpress/d' | sed -e '/drupal8/d' | sed -n '1,3 !p' | sed -n '$ !p' | tr -s ' ' | cut -d ' ' -f-2))"
 # create a custom array by uncommenting and editing the next line:
-# declare -a arr="(copp crim crd spa socialwork)"
+declare -a arr="(spa crim morrison2017 crd cemhs)"
 
 #uncomment next line to check array
 # printf '%s\n' "${arr[@]}"
