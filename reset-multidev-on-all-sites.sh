186645a6318b7dfe9100ed658b3e2760acade98b
Travis Butterfield <travisbutterfield@gmail.com>
2020-11-10 14:33:36 -0700

    clean up more

--- reset-multidev-on-all-sites.sh
+++ reset-multidev-on-all-sites.sh
@@ -1,39 +1,55 @@
 #!/bin/bash
 IFS=$'\n'
- printf "\nHello!\n\nWelcome to my bash script for resetting a multidev environment on all Pantheon sites\n\n"
+
+printf "\nHello!\n\nWelcome to my bash script for resetting a multidev environment on all Pantheon sites\n\n"
 
 read -r -p "Please designate the multidev you want to reset: " multidev
 
-printf 'I will now loop through all the sites and reset the \"%s\" multidev.\n\n' "$multidev"
+printf '\nOpening Pantheon sites in different tabs and resetting the \"%s\" multidev.\n' "$multidev"
+printf 'If \"%s\" does not exist, it will be created.\n\n' "$multidev"
 
 declare -a arr="($(terminus site:list --team --fields=name,framework | sed -e '/wordpress/d' | sed -e '/drupal8/d' | sed -n '1,3 !p' | sed -n '$ !p' | tr -s ' ' | cut -d ' ' -f-2))"
-# declare -a arr=("crim" "crd")
+#declare -a arr=("crim")
 # printf '%s\n' "${arr[@]}"
 for site in "${arr[@]}"
 	do
 		export site
 		export multidev
-		function local_test() {
-			test=$(ls -1a /var/www/html | grep -w "^$site$")
-			if [[ $test != "$site" ]]
-			then
-				giturl="$(terminus connection:info --format="string" --fields=git_url -- "$site".dev)"
-				printf "\n\n...Please wait while I clone the site to your local environment...\n\n"
-				git clone "$giturl" "/var/www/html/$site"
-			fi
-			terminus env:wipe --yes "$site"."$multidev"
-			cd /var/www/html/"$site" || exit
-			git fetch --all
-			git checkout "$multidev"
-			git pull
-			commit_hash="$(git merge-base "$multidev" master)"
-			git reset "$commit_hash" --hard
-			git pull -X theirs origin master
-			git push origin "$multidev" --force # Remove this line if you opt for the force push warning
-			terminus env:clone-content --updatedb --yes -- "$site".live "$multidev"
-			terminus env:clear-cache -- "$site"."$multidev"
-			git checkout master
-		}
-		export -f local_test
-		gnome-terminal --tab --title="$site" -- bash -c "local_test; $SHELL"
+		function resetmultidev() {
+      checkfirst=$(terminus multidev:list --fields=id --format=list -- "$site" | grep "$multidev")
+      #printf "$checkfirst\n\n"
+      #check if multidev exists
+      if [[ "$checkfirst" != "$multidev" ]]
+      then
+        printf 'The \"%s\" multidev does not exist on %s. I will create it now.\n\n' "$multidev" "$site"
+        terminus multidev:create "$site".live "$multidev"
+      #if multidev does exist, reset it
+      else
+        test=$(ls -1a /var/www/html | grep -w "^$site$")
+        #check if multidev has been cloned locally
+        if [[ "$test" != "$site" ]]
+        then
+          giturl="$(terminus connection:info --format="string" --fields=git_url -- "$site".dev)"
+          printf "\n\n...Please wait while I clone the site to your local environment...\n\n"
+          git clone "$giturl" "/var/www/html/$site"
+        fi
+        #wipe multidev
+        terminus env:wipe --yes "$site"."$multidev"
+        cd /var/www/html/"$site" || exit
+        #make local multidev branch match master branch
+        git fetch --all
+        git checkout "$multidev"
+        git pull
+        commit_hash="$(git merge-base "$multidev" master)"
+        git reset "$commit_hash" --hard
+        git pull -X theirs origin master
+        #force push updated multidev to Pantheon
+        git push origin "$multidev" --force
+        terminus env:clone-content --updatedb --yes -- "$site".live "$multidev"
+        terminus env:clear-cache -- "$site"."$multidev"
+        git checkout master
+      fi
+    }
+		export -f resetmultidev
+		gnome-terminal --tab --title="$site" -- bash -c "resetmultidev; $SHELL"
 	done
--- terminus-create-multidev-for-all-sites.sh
+++ terminus-create-multidev-for-all-sites.sh
@@ -1,22 +1,29 @@
 #!/bin/bash
 
-printf "\nHello!\n\nWelcome to my bash script for creating a wsupdt multidev environment for updating webspark!\n\n"
+printf "\nHello!\n\nWelcome to my bash script for creating a multidev environment on all Pantheon sites!\n\n"
 
-read -p 'Please name your multidev instance: ' multidevname
+read -r -p 'Please name your multidev instance: ' multidev
 
-declare -a arr="($(terminus site:list --team --fields=name,framework | sed -e '/wordpress/d' | sed -e '/morrison-institute/d' | sed -e '/azwater/d' | sed -e '/lodestar/d' | sed -e '/copp-community/d' | sed -n '1,3 !p' | sed -n '$ !p' | tr -s ' ' | cut -d ' ' -f-2))"
+declare -a arr="($(terminus site:list --team --fields=name,framework | sed -e '/wordpress/d' | sed -e '/morrison-institute/d' | sed -e '/drupal8/d' | sed -n '1,3 !p' | sed -n '$ !p' | tr -s ' ' | cut -d ' ' -f-2))"
 #create a custom array by uncommenting and editing the next line:
-# declare -a arr=("pubsrvstarter")
+# declare -a arr=("cord")
 
 #uncomment next line to check array
 #printf '%s\n' "${arr[@]}"
 
-for i in "${arr[@]}"
+for site in "${arr[@]}"
 do
-  gnome-terminal --tab --title="$i" -- bash -c 'printf "..... '"$i"' .....\n"; terminus multidev:create '"$i"'.live '"$multidevname"'; printf "....................\n\n"; $SHELL'
-  # printf "..... $i .....\n";
-  # terminus multidev:create "$i".live $multidevname;
-  # printf "....................\n\n";
+  function checkmultidevs() {
+    checkfirst=$(terminus multidev:list --fields=id --format=list -- "$site" | grep "$multidev")
+#    printf "$checkfirst\n\n"
+    if [[ "$checkfirst" != "$multidev" ]]
+    then
+      gnome-terminal --tab --title="$site" -- bash -c "printf '..... %s .....\n' ""$site""; terminus multidev:create $site.live $multidev; printf '....................\n\n'; $SHELL"
+    else
+      printf 'The \"%s\" multidev already exists on %s\n\n' "$checkfirst" "$site"
+    fi
+  }
+  checkmultidevs
 done
 
 unset arr
